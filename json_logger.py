f60e576947f36179d22d259e55ed4dce41241a2f
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-02-11 17:08:44 -0600

    works

--- json_logger.py
+++ json_logger.py
@@ -1,8 +1,8 @@
-''' [======- - - - -=================- All Utilities Standard -=================- - - - -======] '''
-# to allow for relative imports
-import sys, os
-sys.path.insert(1, os.path.join(sys.path[0], os.path.dirname(os.path.abspath(__file__))))
-''' [======- - - - - - -=============- - - - -========- - - - -=============- - - - - - -======] '''
+# ''' [======- - - - -=================- All Utilities Standard -=================- - - - -======] '''
+# # to allow for relative imports
+# import sys, os
+# sys.path.insert(1, os.path.join(sys.path[0], os.path.dirname(os.path.abspath(__file__))))
+# ''' [======- - - - - - -=============- - - - -========- - - - -=============- - - - - - -======] '''
 
 
 import json
