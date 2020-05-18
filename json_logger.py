af4d7f5ea53d36af557562fb6c74fdcd857fd455
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-05-17 20:30:42 -0500

    switched headers to rel imports

--- json_logger.py
+++ json_logger.py
@@ -1,27 +1,13 @@
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Header -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
-import sys, os    ;     sys.path.insert(1, os.path.join(sys.path[0], os.path.dirname(os.path.abspath(__file__)))) # to allow for relative imports, delete any imports under this line
- 
-util_submodule_l = ['file_system_utils']  # list of all imports from local util_submodules that could be imported elsewhere to temporarily remove from sys.modules
- 
-# temporarily remove any modules that could conflict with this file's local util_submodule imports
-og_sys_modules = sys.modules    ;    pop_l = [] # save the original sys.modules to be restored at the end of this file
-for module_descrip in sys.modules.keys():  
-    if any( util_submodule in module_descrip for util_submodule in util_submodule_l )    :    pop_l.append(module_descrip) # add any module that could conflict local util_submodule imports to list to be removed from sys.modules temporarily
-for module_descrip in pop_l    :    sys.modules.pop(module_descrip) # remove all modules put in pop list from sys.modules
-util_submodule_import_check_count = 0 # count to make sure you don't add a local util_submodule import without adding it to util_submodule_l
- 
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard: Local Utility Submodule Imports  -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
- 
-from util_submodules.file_system_utils import file_system_utils as fsu    ; util_submodule_import_check_count += 1
- 
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
-if util_submodule_import_check_count != len(util_submodule_l)    :    raise Exception("ERROR:  You probably added a local util_submodule import without adding it to the util_submodule_l")
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
- 
- 
 import jsonplus__non_merged as json
 import os.path
  
+ 
+if __name__ == "__main__": 
+    from   usms.file_system_utils import file_system_utils as fsu
+else:
+    from . usms.file_system_utils import file_system_utils as fsu
+    
+    
  
 # changes values of headers already in the file, and appends lines with new headers to the end, or creates new file if dosn't already exist
 def log_vars(log_dict, json_file_path):
@@ -80,9 +66,7 @@ def read_fast(json_file_path):
     return data
  
  
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Footer -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
-sys.modules = og_sys_modules
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
+
 if __name__ == '__main__':
     print('In Main:  json_logger')
      
